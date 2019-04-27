af124232e3b3e1679a40f338d09fc4c5a5f6ddfd
pashik913 <45880699+pashik913@users.noreply.github.com>
2019-04-27 11:33:11 +0300

    выпадeние меню из потока

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,10 @@
-var nojs = document.querySelector('.page-header--nojs');
 var burger = document.querySelector('.page-header__toggle');
 var header = document.querySelector('.page-header');
 
 
-  nojs.classList.remove('page-header--nojs');
+  header.classList.remove('page-header--nojs');
 
   burger.addEventListener('click', function () {
 
-    console.log('click-menu');
     header.classList.toggle('page-header--opened');
   })
