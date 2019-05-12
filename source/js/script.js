3bb5991e93f5887b4ed3edf1ab4e032cb5a795ec
pashik913 <45880699+pashik913@users.noreply.github.com>
2019-05-12 14:27:05 +0300

    правки по замечаниям наставника

--- source/js/script.js
+++ source/js/script.js
@@ -4,17 +4,16 @@ var popup = document.querySelector('.modal-link-js');
 var modal = document.querySelector('.modal');
 var close = document.querySelector('.modal__button');
 
-  header.classList.remove('page-header--nojs');
+header.classList.remove('page-header--nojs');
 
-  burger.addEventListener('click', function () {
-    header.classList.toggle('page-header--opened');
-  })
+burger.addEventListener('click', function () {
+  header.classList.toggle('page-header--opened');
+})
 
-  popup.addEventListener('click', function () {
-    modal.classList.toggle('modal--show');
-  })
+popup.addEventListener('click', function () {
+  modal.classList.toggle('modal--show');
+})
 
-
-  close.addEventListener('click', function () {
-    modal.classList.remove('modal--show');
-  })
+close.addEventListener('click', function () {
+  modal.classList.remove('modal--show');
+})
